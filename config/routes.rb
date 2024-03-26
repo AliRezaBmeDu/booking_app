d4ff474b383b7391ceca2d4f7a1567db90ac3986
AliRezaBmeDu <ali.reza.buet@gmail.com>
2024-03-26 09:57:17 +0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  namespace :api do
-    post '/registration' to: 'registrations#create'
-  end
   
+  namespace :api do
+    post '/register', to: 'registrations#create'
+  end
+
   resources :users do
     resources :posts do
       resources :comments
     end
   end
 
+  root 'registrations#index'
 end
